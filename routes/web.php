b959ad2f2a8d0681a25a259c5baf4aa70308c5c2
Student name <HemilDudhat2001@gmail.com>
2024-04-03 11:20:06 +0530

    second commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('index');
+});
+
+
+Route::fallback(function () {
+    return "404 Not Found";
 });
